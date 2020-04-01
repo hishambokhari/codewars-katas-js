1ab43f57d59c58dc2bfe817a7c1a1a13ab983d65
root <hish.b92@gmail.com>
2020-04-01 20:14:39 +0500

    even

---
+++ even.js
@@ -0,0 +1,13 @@
+// Create a function (or write a script in Shell) 
+// that takes an integer as an argument and returns 
+// "Even" for even numbers or "Odd" for odd numbers.
+
+function even_or_odd(number) {
+  if (number % 2 == 0) {
+    return "Even"
+  } else if (number % 2 != 0) { 
+    return "Odd"
+  }
+}
+
+console.log(even_or_odd(1))
