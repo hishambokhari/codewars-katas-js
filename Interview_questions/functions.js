c7d5020aca5623e742acebc32a358effef0f9984
hishambokhari <hish.b92@gmail.com>
2020-06-12 17:31:04 +0500

    functions

--- Interview_questions/functions.js
+++ Interview_questions/functions.js
@@ -1,5 +1,44 @@
-function greet(firstName, lastName){
-    return 'Hello ' + firstName + ' ' + lastName;
+
+// // function declarations
+// function greet(firstName= "Jon", lastName= "Doe"){
+//     return 'Hello ' + firstName + ' ' + lastName;
+// }
+
+// console.log(greet());
+
+// Function Expressions
+
+// const square = function(x){
+//     return x*x;
+// };
+
+// console.log(square(8));
+
+// Immidiatley Invokable function expressions - IIFEs
+
+// (function(){
+//     console.log('IIFE Ran...');
+// })();
+
+// (function(name){
+//     console.log('Hello ' + name );
+// })('Brad'); // useful for design patterns ( module patterns)
+
+// property methods
+
+const todo = {
+    add: function(){
+        console.log('Add todo...');
+    },
+    edit: function(id){
+        console.log(`Edit todo ${id}`);
+    }
 }
 
-console.log(greet('Hisham', 'Bokhari'));
+todo.delete = function(){
+    console.log('Delete todo');
+}
+
+todo.add();
+todo.edit(22);
+todo.delete();
