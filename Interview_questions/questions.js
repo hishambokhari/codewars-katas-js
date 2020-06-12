cd01dfff6c10586c0ecbb4d7ca205d8d61625b57
hishambokhari <hish.b92@gmail.com>
2020-06-12 15:23:17 +0500

    questions

---
+++ Interview_questions/questions.js
@@ -0,0 +1,31 @@
+// Given 2 arrays, create a function that lets a user know (true/false)
+// whether these two arrays contain any common items. 
+
+
+
+// for example:
+const array1 = ['a','b','c','x']
+const array2 = ['z','y','x']
+//should return false
+
+// const array1 = ['a','b','c','x']
+// const array2 = ['z','y','x']
+// should return true
+
+//solution:
+
+// 2 parameters - arrays - no size limit
+// return true or false
+
+function containsCommonItem(arr1,arr2) {
+    for (let i=0; i < arr1.length; i++) {
+        for (let j=0; j < arr2.length; j++) {
+            if (arr1[i] === arr2[j]) {
+                return true;
+            }
+        }
+    }
+    return false
+}
+
+console.log(containsCommonItem(array1, array2));
