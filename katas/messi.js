0c1e100169e1ed15494d62c415fe42ff8ffbaf94
hishambokhari <hish.b92@gmail.com>
2020-06-13 00:01:08 +0500

    katas

---
+++ katas/messi.js
@@ -0,0 +1,22 @@
+// Messi goal scoring statistics:
+
+// Competition	Goals
+// La Liga	43
+// Champions League	10
+// Copa del Rey	5
+// Define
+// Create three variables and store the appropriate values using the table above:
+
+// laLigaGoals
+// championsLeagueGoals
+// copaDelReyGoals
+// Create a fourth variable named totalGoals (javascript, C#, Java) or total_goals (python, ruby).
+//  totalGoals/total_goals should store the sum of all of Messi's goals for this year.
+
+const laLigaGoals = 43
+const championsLeagueGoals = 10
+const copaDelReyGoals = 5
+
+const totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
+
+console.log(totalGoals);
