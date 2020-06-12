68d765bc9f23e42c1bb7fcd9aa23b931635cda8a
hishambokhari <hish.b92@gmail.com>
2020-06-12 18:03:46 +0500

    loops

---
+++ Interview_questions/loops.js
@@ -0,0 +1,40 @@
+//  For loops
+
+// for(let i=0; i < 10; i++ ){
+//     if(i==2){
+//         console.log('2 is my fav no.');
+//         // continue; // so that number 2 is replaced with the above line
+//         // break; // stops at 2 is my fav no.
+//     }
+//      console.log('Number ' + i);
+// }
+
+// While loops
+
+// let i = 0;
+
+// while(i < 10){
+//     console.log('Number ' + i);
+//     i++;
+// }
+
+// Do while loop
+
+// let i = 0;
+
+// do {
+//     console.log('Number '+ i);
+//     i++;
+// }
+
+// while(i < 10);
+
+// looping with arrays - loop through arrays
+
+const cars = ['ford', 'chevy', 'honda', 'toyota'];
+
+
+// for(let i=0; i<cars.length; i++){
+//     console.log(cars[i]);
+// }
+
