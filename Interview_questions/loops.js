00ecc89dd8e8939761947d711b2d873dc47af885
hishambokhari <hish.b92@gmail.com>
2020-06-12 22:26:54 +0500

    Scopes

--- Interview_questions/loops.js
+++ Interview_questions/loops.js
@@ -69,3 +69,26 @@
 //     console.log(`${x} : ${user[x]}`);
 // }
 
+
+// global scope
+
+// var a = 1;
+// let b = 2;
+// const c = 3;
+
+// function test(){
+//     var a = 4;
+//     let b = 5;
+//     const c = 6;
+
+//     console.log('Function Scope: ', a, b, c);
+// }
+
+// test();
+
+
+
+// console.log('Global Scope: ', a, b, c);
+
+
+
