fed397d5b3fb7c03447b5f64eaf7555f24a238eb
hishambokhari <hish.b92@gmail.com>
2020-06-14 16:13:44 +0500

    Events

--- Events/myscript.js
+++ Events/myscript.js
@@ -11,3 +11,13 @@ headOne.addEventListener('mouseout', function(){
     headOne.textContent = "HOVER OVER ME!"
     headOne.style.color = 'black';
 })
+
+headTwo.addEventListener('click', function(){
+    headTwo.textContent = 'CLICKED ON';
+    headTwo.style.color = 'blue';
+})
+
+headThree.addEventListener('dblclick', function(){
+    headThree.textContent = "I was double clicked";
+    headThree.style.color = 'green';
+})
