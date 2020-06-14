c115e65013a6bc8cc8c4fd3fef28025842b4d3c4
hishambokhari <hish.b92@gmail.com>
2020-06-15 00:21:54 +0500

    add to array

---
+++ Interview_questions/array.js
@@ -0,0 +1,14 @@
+// var myArray = ['a','b','c','d'];
+
+// myArray.unshift('g') // to add to the beginging of an array
+
+// myArray.push('h') //adding to the end of the array
+
+// myArray = ["start",...myArray] // spread operator
+
+// myArray = [...myArray, "end"] // end of the array
+
+
+// console.log(myArray)
+
+
