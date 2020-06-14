4965b8bf8f6d3f08b6c60a1db8a748ac42ab68f9
hishambokhari <hish.b92@gmail.com>
2020-06-14 22:02:33 +0500

    Jquery

---
+++ Jquery/myscript.js
@@ -0,0 +1,27 @@
+// $('h1').click(function(){
+//     $(this).text("I was changed")
+// })
+
+// Key press
+
+// $('input').eq(0).keypress(function(event){
+//     if(event.which === 13) {
+//         $('h3').toggleClass('turnBlue');
+//     }
+// })
+
+// On method
+
+// $('h1').on('mouseenter', function(){
+//     $(this).toggleClass('turnBlue');
+// })
+
+//Events and animations
+
+// $('input').eq(1).on('click',function(){
+//     $('.container').fadeOut(3000)
+// })
+
+// $('input').eq(1).on('click',function(){
+//     $('.container').slideUp(3000)
+// })
