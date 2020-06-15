6bd8ff49b69a972c9581c83b5b11c149a0d5537f
hishambokhari <hish.b92@gmail.com>
2020-06-15 16:37:57 +0500

    q4

---
+++ js_interview/q4.js
@@ -0,0 +1,37 @@
+// What is the diff between function decleration and function expression?
+
+// variable in function decleration but no var in function expression
+// if you console.log before the function function decleration funciton will be returned  
+// but expression wont because expression is in a var scope.
+//
+
+// what is promises and why do we use it?
+
+// watch tutorial
+
+// what is closure and how do we use it?
+
+// when a function returns anoter function
+// the returning function will hold its environment
+// basically all the variables its needed
+
+// let obj = function(){
+//     let i = 0;
+
+
+//     return {
+//         setI(k){
+//             i = k;
+//         },
+//         getI(){
+//             return i;
+//         }
+//     }
+// };
+
+// let x = obj();
+
+// x.setI(2);
+// console.log(x.getI());
+
+// watch tutorial on closure
