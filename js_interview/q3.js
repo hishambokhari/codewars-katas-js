19c61ad8894bb2936de5c3504387c9856aaa2580
hishambokhari <hish.b92@gmail.com>
2020-06-15 13:36:47 +0500

    q5

---
+++ js_interview/q3.js
@@ -0,0 +1,29 @@
+// question 4 
+// diff between undefined and null?
+
+// they both represent empty values
+
+//  when u define a var and dont assign a value to it 
+// it automatically puts a placeholder called undefined
+// null = you are doing it yourself. 
+// 
+
+// question 5 
+// what is the use of arrow functions
+
+// const profile = {
+//     firstName: '',
+//     lastName: '',
+//     setName: function(name){
+//         let splitName = (n) => {
+//             let nameArray = n.split(' ');
+//             this.firstName = nameArray[0];
+//             this.lastName = nameArray[1];
+//         }
+//         splitName(name);
+//     }
+// }
+// profile.setName('john doe');
+// console.log(firstName);
+
+// checkout the tutorial on arrow functions
