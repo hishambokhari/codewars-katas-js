a8bf6d3de78148a95fa4e53370a22a3e4f51a750
hishambokhari <hish.b92@gmail.com>
2020-06-15 13:21:08 +0500

    q2 and q3

---
+++ js_interview/q2.js
@@ -0,0 +1,32 @@
+// Question 2 
+
+// what is the difference between "==" and "==="?
+
+// Both these signs are comparison operators
+
+// Difference:
+
+// "==" only compares the value not the type
+
+// "===" compares value and type
+
+// if('1' == 1){
+
+// }
+
+// question 3
+
+// What is the difference between keyword let and const?
+
+// Both can be used to define variables
+
+// with const after the first assignment of value you cannot reassign it 
+// let allows you to assign values as many times 
+
+// let l = 1;
+// l=2;
+// console.log(l); reassigns the l value to 2 
+
+// const c = 1;
+// c=2;
+// console.log(c); throws an error because cannot reassign 
