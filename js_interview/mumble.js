9261e571234a8cd9e205834e33b1b2712e2b0395
hishambokhari <hish.b92@gmail.com>
2020-06-16 17:09:00 +0500

    kata

---
+++ js_interview/mumble.js
@@ -0,0 +1,41 @@
+// This time no story, no theory. The examples below show you how to write function accum:
+
+// Examples:
+
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+function accum(input) {
+    //place to store results
+    let result = '';
+    // iterate over the string
+    for(i=0;i<input.length;i++){
+        for(let j = 0; j < i + 1;j++){
+            if (j == 0){
+                result += input[i].toUpperCase();
+            } else {
+                result += input[i].toLowerCase();
+            }
+        }
+        if(i != input.length - 1){
+            result += '-'
+        }
+    }
+    // append the current letter i+1 times to the string
+    // capitalize the first append
+    // append a dash if it is not the last letter
+    return result;
+}
+
+console.log(accum("abcd"))
+
+// Test.describe("accum",function() {
+//     Test.it("Basic tests",function() {    
+//         Test.assertEquals(accum("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+//         Test.assertEquals(accum("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
+//         Test.assertEquals(accum("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
+//         Test.assertEquals(accum("EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
+//         Test.assertEquals(accum("HbideVbxncC"), "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
+//     })})
