9e794376aa17027781c7778b7d3481dde3793798
hishambokhari <hish.b92@gmail.com>
2020-06-17 23:49:38 +0500

    array

--- arrays/positive.js
+++ arrays/positive.js
@@ -4,17 +4,19 @@
 
 // Note: if there is nothing to sum, the sum is default to 0.
 
-
-function positiveSum(arr) {
+function positiveSum(arr){
 
     var total = 0;
-  for(i = 0; i < arr.length; i++){
-    if(arr[i] > 0){
-        total += arr[i];
-    } 
-  }  return total;
+
+        for(i = 0; i < arr.length; i++){
+            if(arr[i] > 0){
+                total += arr[i]
+            }
+        }
+        return total
 }
-   
+
+console.log(positiveSum([1,-2,3,4,5]));
 
 
 // Test.assertEquals(positiveSum([1,2,3,4,5]),15);
