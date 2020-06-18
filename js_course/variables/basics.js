dc9cea8d1a962ac2b2cda2a76385526018c2082f
hishambokhari <hish.b92@gmail.com>
2020-06-18 14:02:46 +0500

    const

--- js_course/variables/basics.js
+++ js_course/variables/basics.js
@@ -1,8 +1,14 @@
 // Declaring a variable
 
 
-// using let
+// using let - allows for reassigning 
 let myDogsName = "Zeus";
-
+myDogsName = 'Lui' // reassigning dogs name from zeus to lui
 console.log(myDogsName);
+
+// using const - locks the variable - cannot reassign
+
+const myDogsNewName = "Roger";
+// myDogsNewName = "Timmy" --  this will give an error
+console.log(myDogsNewName);
 
