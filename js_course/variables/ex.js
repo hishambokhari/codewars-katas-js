d3649bf1e88b87e8271f56cfa6b3f11f4a24119b
hishambokhari <hish.b92@gmail.com>
2020-06-18 16:14:01 +0500

    rev

---
+++ js_course/variables/ex.js
@@ -0,0 +1,29 @@
+
+
+// console.log(x.length); // length of x is 23
+
+// let y = "love"
+
+// console.log(y[0]); // first index "l"
+// console.log(y[3]); // last index "e"
+
+// y = "bunny" // replace love with bunny
+
+// console.log(y);
+
+// console.log(x[12]); // char at index 12
+
+// let y = 25.555
+
+// let len = Math.floor(y);
+
+// let toStr = len.toString();
+
+// console.log(Boolean(y));
+
+
+
+// reverse the string such that you get the array in reverse order
+// let x = "I love loving your love"
+
+// console.log(x.split(" ").reverse().join(" "));
