f4a0b404e626215622570ee1321d0dbfa811db65
hishambokhari <hish.b92@gmail.com>
2020-06-18 15:54:11 +0500

    booleans

---
+++ js_course/variables/conversions.js
@@ -0,0 +1,20 @@
+// convert to strings
+
+// let num = 20.20;
+// let numString = num.toString();
+
+// cannot convert null or undefined to String
+
+// can convert boolean to string
+
+// convert to number
+
+// let str = "10"; // number string can be converted using console.log(+str);
+
+// boolean
+
+let num = '20';
+
+console.log(Boolean(num));
+
+
