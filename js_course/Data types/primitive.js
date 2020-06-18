53216a3a09abab8ed8f430ea2a5109064e695b45
hishambokhari <hish.b92@gmail.com>
2020-06-18 14:17:37 +0500

    types

---
+++ js_course/Data types/primitive.js
@@ -0,0 +1,19 @@
+// undefined
+
+let myDogsName;
+
+myDogsName = 'cujo';
+
+// console.log(myDogsName); // gives undefined
+
+
+// type of
+
+
+
+
+// null - where you manually give a value of null
+
+myDogsName = null; // type of null is an object
+
+console.log(typeof(myDogsName));
