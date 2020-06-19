f4ec16f9ac2e413792c7f1ce3eae402536cb0995
hishambokhari <hish.b92@gmail.com>
2020-06-19 13:35:41 +0500

    loops

---
+++ js_course/variables/loops.js
@@ -0,0 +1,12 @@
+// for(let i = 1;i <= 100;i++){ // print numbers between 1 to 100
+//   console.log(i)
+// }
+
+
+// for(let i = 2;i <= 100;i++,i++){ // print even numbers between 1 and 100
+//   console.log(i)
+// }
+
+// for(let i = 2;i <= 100;i+=4){ // print numbers with increments of 4 between 1 and 100
+//   console.log(i)
+// }
