a1a23d2bd853aaff91f2cc0514e676b06f136191
hishambokhari <hish.b92@gmail.com>
2020-06-20 14:39:51 +0500

    obj

--- js_course/variables/l.js
+++ js_course/variables/l.js
@@ -1,14 +1,33 @@
-const  gList = ["bread","milk","eggs","tomato"];
+// const  gList = ["bread","milk","eggs","tomato"];
 
-// print all the names below
+// // print all the names below
 
-let len = gList.length;
+// let len = gList.length;
 
-// for(let i = 0; i <= len -1;i++){
-//   console.log(`${i} - ${gList[i]}`)
-// }
+// // for(let i = 0; i <= len -1;i++){
+// //   console.log(`${i} - ${gList[i]}`)
+// // }
 
-// gList.forEach((item, index)=> {
-//   console.log(`${index} - ${item}`)
-// })
+// // gList.forEach((item, index)=> {
+// //   console.log(`${index} - ${item}`)
+// // })
 
+//Objects
+
+const profile = {
+  name: "john",
+  age: 30,
+  single: true,
+  "got a job": true,
+  kids: [{
+    name: "peter",
+    age: 3
+  }, {
+    name: "kate",
+    age: 2
+  }]
+};
+
+profile.kids.push({name: "joe",age: 5})
+
+console.log(profile.kids)
