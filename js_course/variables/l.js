e12a07314ee3901651de23e4d676f97aa9f38e68
hishambokhari <hish.b92@gmail.com>
2020-06-20 14:32:57 +0500

    elements of array

---
+++ js_course/variables/l.js
@@ -0,0 +1,14 @@
+const  gList = ["bread","milk","eggs","tomato"];
+
+// print all the names below
+
+let len = gList.length;
+
+// for(let i = 0; i <= len -1;i++){
+//   console.log(`${i} - ${gList[i]}`)
+// }
+
+// gList.forEach((item, index)=> {
+//   console.log(`${index} - ${item}`)
+// })
+
