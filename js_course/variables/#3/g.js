0cc7ff7ebb563676b3e529d8c72d7373257e149f
hishambokhari <hish.b92@gmail.com>
2020-06-22 01:15:24 +0500

    e

--- js_course/variables/#3/g.js
+++ js_course/variables/#3/g.js
@@ -1,3 +1,4 @@
+//q1
 // function numbers(x,y){
 //   if (x == 100 || (x + y == 100)){
 //     return true;
@@ -7,9 +8,20 @@
 // }
 
 // console.log(numbers(10,90));
-
+//q2
 // write a function in js to get the extension of a filename
 
-const fileName = (str) => str.slice
-(str.lastIndexOf('.'));
-console.log(fileName('index.html'));
+// const fileName = (str) => str.slice
+// (str.lastIndexOf('.'));
+// console.log(fileName('index.html'));
+
+//q3
+// write a js program to replace every character in a given string with the character following it in the alphabet
+
+const moveForward = (str) =>
+str.split('')
+.map(char => String.fromCharCode(char.charCodeAt(0) + 1
+))
+.join('');
+
+console.log(moveForward("a,b,c,d"))
