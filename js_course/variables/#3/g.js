385da2f5aff69a050f1852122ea80d1dd29f2160
hishambokhari <hish.b92@gmail.com>
2020-06-22 01:05:45 +0500

    file name

--- js_course/variables/#3/g.js
+++ js_course/variables/#3/g.js
@@ -1,9 +1,15 @@
-function numbers(x,y){
-  if (x == 100 || (x + y == 100)){
-    return true;
-  } else {
-    return false;
-  }
-}
+// function numbers(x,y){
+//   if (x == 100 || (x + y == 100)){
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
 
-console.log(numbers(10,90));
+// console.log(numbers(10,90));
+
+// write a function in js to get the extension of a filename
+
+const fileName = (str) => str.slice
+(str.lastIndexOf('.'));
+console.log(fileName('index.html'));
