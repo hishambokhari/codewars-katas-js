974f4c5a5566db3d753382fcc74ceded8c3cdf25
hishambokhari <hish.b92@gmail.com>
2020-06-23 12:51:47 +0500

    promises

--- js_course/variables/#3/async.js
+++ js_course/variables/#3/async.js
@@ -1,3 +1,43 @@
 // promises
 
 
+const willCleanTheRoom = new Promise((resolve, reject) =>{
+
+    let cleanRoom = true;
+    if (cleanRoom){
+      resolve('room is clean')
+    } else {
+      reject('Room is still dirty')
+    }  
+});
+
+const removeGarbage = new Promise((resolve, reject)=> {
+  let removeGarbage = true;
+  if(removeGarbage){
+    resolve('garbage removed');
+  } else {
+    reject('garbage is not removed');
+  }
+});
+
+const getReward = new Promise((resolve, reject)=> {
+  let getReward = true;
+  if(getReward){
+    resolve('recieved reward');
+  } else {
+    reject(':( no rewards')
+  }
+})
+
+willCleanTheRoom.then(resolveStatus => {
+  console.log(resolveStatus)
+  return removeGarbage;
+}).then(resolveStatus => {
+  console.log(resolveStatus);
+  return getReward;
+}).then(resolveStatus => {
+  console.log(resolveStatus);
+  console.log('all done');
+}).catch(rejectStatus => {
+  console.log(rejectStatus)
+})
