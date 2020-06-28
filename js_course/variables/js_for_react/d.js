300a161e3f905f03e6936b6b238660c3e80cfa59
hishambokhari <hish.b92@gmail.com>
2020-06-29 01:13:03 +0500

    fuel

---
+++ js_course/variables/js_for_react/d.js
@@ -0,0 +1,29 @@
+// You were camping with your friends far away from home, 
+// but when it's time to go back, you realize that your fuel is running 
+// out and the nearest pump is 50 miles away! You know that on average, 
+// your car runs on about 25 miles per gallon. There are 2 gallons left. 
+// Considering these factors,
+//  write a function that tells you if it is possible to get to the pump or not. 
+//  Function should return true (1 in Prolog) if it is possible and false (0 in Prolog) if not. 
+//  The input values are always positive.
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+    
+   
+  if (fuelLeft*mpg >= distanceToPump) {
+      return true;
+  } else if(fuelLeft*mpg  >= mpg) {
+      return true
+  } else {
+      return false
+  }
+}
+
+// const assert = require("chai").assert;
+
+// describe("zeroFill", function() {
+//   it("Sample Tests", function() {
+//     assert.equal(zeroFuel(50, 25, 2), true);
+//     assert.equal(zeroFuel(100, 50, 1), false);
+//   });
+// });
