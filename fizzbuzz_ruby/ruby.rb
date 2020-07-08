28e6bac04435c689a897e6d5de905ec88e743df3
hishambokhari <hish.b92@gmail.com>
2020-07-08 14:09:35 +0500

    Hashed fizzBuzz

--- fizzbuzz_ruby/ruby.rb
+++ fizzbuzz_ruby/ruby.rb
@@ -1,13 +1,33 @@
 #Basic Fizzbuzz in ruby:
 
-1.upto(100) do |i|
-  if i % 3 && i % 5 == 0
-    puts "FizzBuzz"
-  elsif i % 3 == 0
-    puts "Fizz"
-  elsif i % 5 == 0
-    puts "Buzz"
-  else 
-    puts i
+# 1.upto(100) do |i|
+#   if i % 3 && i % 5 == 0
+#     puts "FizzBuzz"
+#   elsif i % 3 == 0
+#     puts "Fizz"
+#   elsif i % 5 == 0
+#     puts "Buzz"
+#   else 
+#     puts i
+#   end
+# end
+
+# Hashed FizzBuzz in Ruby
+
+def hashed_fizz_buzz num
+  hash = Hash.new
+  1.upto(num).each do |i|
+    if (i%3==0) && (i %5==0)
+      hash[i] = "FizzBuzz"
+    elsif (i%3==0)
+      hash[i] = "Fizz"
+    elsif (i%5==0)
+      hash[i] = "Buzz"
+    else 
+      hash[i] = i
+    end
   end
+  hash
 end
+
+puts(hashed_fizz_buzz(16))
