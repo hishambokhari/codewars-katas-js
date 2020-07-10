7aa8146c4ae587b61e3cc9a3f0ec03ec20708260
hishambokhari <hish.b92@gmail.com>
2020-07-11 00:50:14 +0500

    7kyu

---
+++ new_katas/d.js
@@ -0,0 +1,20 @@
+// Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+
+// Example:
+
+// "riley" --> "Hello Riley!"
+// "JACK"  --> "Hello Jack!"
+
+var greet = function(name) {
+
+  const properName = name[0].toUpperCase() + name.slice(1).toLowerCase()
+  return `Hello ${properName}!`
+};
+console.log(greet("JACK"))
+
+// Create your own tests here. These are some of the methods available:
+//  Test.expect(boolean, [optional] message) 
+//  Test.assertEquals(actual, expected, [optional] message)
+//  Test.assertSimilar(actual, expected, [optional] message)
+//  Test.assertNotEquals(actual, expected, [optional] message) 
+// Test.assertEquals(greet('riley'), 'Hello Riley!');
