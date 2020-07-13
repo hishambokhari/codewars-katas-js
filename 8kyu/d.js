7d3f1dd792a1b9b2a54b31af8ee0a5955bda5945
hishambokhari <hish.b92@gmail.com>
2020-07-13 12:33:45 +0500

    8kyu

---
+++ 8kyu/d.js
@@ -0,0 +1,9 @@
+// Implement a function which convert the given boolean value into its string representation.
+
+function booleanToString(b){
+  const btoString = b.toString()
+  return btoString
+}
+
+// Test.assertEquals(booleanToString(true), "true", 'When we pass in true, we want the string "true" as output');
+// Test.assertEquals(booleanToString(false), "false", 'When we pass in false, we want the string "false" as output');
