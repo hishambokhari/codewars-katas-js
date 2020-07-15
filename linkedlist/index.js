162f34158a907bf26fc3adb7de3de619ee009b09
hishambokhari <hish.b92@gmail.com>
2020-07-15 16:02:02 +0500

    ll

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -17,11 +17,43 @@ class LinkedList {
   // Insert first node
   insertFirst(data) {
     this.head = new Node(data, this.head);
+    this.size++;
   }
 
   //Insert last node
+  insertLast(data){
+    let node = new Node(data);
+    let current;
+
+    // if empty, then make it head
+    if (!this.head){
+      this.head = node;
+    } else {
+      current = this.head;
+
+      while(current.next){
+        current = current.next;
+      }
+
+      current.next = node;
+    }
+
+    this.size++
+  }
 
   //Insert at index
+  insertAt(data, index){
+    //if index is out of range
+    if(index > 0 && index > this.size){
+      return;
+    }
+
+    // if first index
+    if(index === 0) {
+      this.head = new Node(data, this.head);
+      return;
+    }
+  }
 
   // Get at index
 
@@ -30,6 +62,14 @@ class LinkedList {
   //Clear list
 
   // Print list data
+  printListData(){
+    let current = this.head;
+
+    while(current) {
+      console.log(current.data);
+      current = current.next;
+    }
+  }
 
 }
 
@@ -37,5 +77,8 @@ const ll = new LinkedList();
 
 ll.insertFirst(100);
 ll.insertFirst(200);
+ll.insertFirst(300);
+ll.insertLast(400)
 
-console.log(ll)
+
+ll.printListData();
