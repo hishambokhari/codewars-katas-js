4dde7783ece7ff22611dbc8e8e52a16e0d5ed19a
hishambokhari <hish.b92@gmail.com>
2020-07-16 15:05:19 +0500

    bst search

--- linkedlist/practice.js
+++ linkedlist/practice.js
@@ -1,7 +1,27 @@
 
 let array = [3,4,6,2,3,645,2,3,6,83,23,13];
 
-sorted = array.sort(function(a,b){
-  return a-b
-})
+function binary_search(arr, value){
+  let high = arr.length - 1;
+  let low = 0;
+  let mid = 0;
+
+  while(low <= high) {
+     mid = Math.floor((high + low) / 2)
+     if (arr[mid] == value){
+       return arr[mid];
+     }else if (value > arr[mid]){
+        low = mid  + 1;
+     } else {
+       high = mid - 1;
+     }
+  }
+  return -1;
+}
+
+let sorted = array.sort(function(a,b) {return a-b});
 console.log(sorted);
+
+let wasItFound = binary_search(sorted, 3);
+
+console.log(wasItFound);
