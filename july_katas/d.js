ce510a01a78306bc7232db4e97a2aea0297459f6
hishambokhari <hish.b92@gmail.com>
2020-07-21 02:22:17 +0500

    creating linked list

---
+++ july_katas/d.js
@@ -0,0 +1,18 @@
+//Linked list
+
+
+// creating a linked list
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null
+    }
+    this.tail = this.head;
+    this.length = 1;
+  }
+}
+
+const myLinkedList = new LinkedList(10)
+
+console.log(myLinkedList)
