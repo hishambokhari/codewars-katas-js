c119cf0fa60c83b74274dae7f2c5985825964a99
hishambokhari <hish.b92@gmail.com>
2020-07-20 22:24:47 +0500

    katas

---
+++ july_katas/a.js
@@ -0,0 +1,49 @@
+// Given two integers a and b, which can be positive or negative, find the sum of all the numbers between including them too and return it. If the two numbers are equal return a or b.
+
+// Note: a and b are not ordered!
+
+// Examples
+// GetSum(1, 0) == 1   // 1 + 0 = 1
+// GetSum(1, 2) == 3   // 1 + 2 = 3
+// GetSum(0, 1) == 1   // 0 + 1 = 1
+// GetSum(1, 1) == 1   // 1 Since both are same
+// GetSum(-1, 0) == -1 // -1 + 0 = -1
+// GetSum(-1, 2) == 2  // -1 + 0 + 1 + 2 = 2
+
+function getSum(a, b){
+   
+  // find lowest of a and b
+  // find highest of a and b
+  let begin;
+  let end;
+  if (a<b){
+    begin = a
+    end = b;
+  } else {
+    begin = b
+    end = a;
+  }
+
+  let sum = 0;
+
+  for(let i = begin;i <= end; i++){
+    sum += i;
+  }
+  return sum;
+  
+
+  // place to store the sum
+
+  // itterate from the lowest to the highest
+  // add current index to the sum
+
+  //return the sum
+}
+console.log(getSum(0,-1))
+
+// Test.describe("Basic Tests:", function(){
+       
+//   Test.assertEquals(getSum(0,-1),-1);
+//   Test.assertEquals(getSum(0,1),1);
+  
+// });
