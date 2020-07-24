2631e5280a640e4961f19f74ecd96d98cf067e8f
hishambokhari <hish.b92@gmail.com>
2020-07-24 19:59:46 +0100

    q

--- js_interview/ques/a.js
+++ js_interview/ques/a.js
@@ -1,6 +1,12 @@
-// In this Kata, you will be given two positive integers a and b and your task will be to apply the following operations:
+// // In this Kata, you will be given two positive integers a and b and your task will be to apply the following operations:
 
-// i) If a = 0 or b = 0, return [a,b]. Otherwise, go to step (ii);
-// ii) If a ≥ 2*b, set a = a - 2*b, and repeat step (i). Otherwise, go to step (iii);
-// iii) If b ≥ 2*a, set b = b - 2*a, and repeat step (i). Otherwise, return [a,b].
-// More examples in tests cases. Good luck
+// // i) If a = 0 or b = 0, return [a,b]. Otherwise, go to step (ii);
+// // ii) If a ≥ 2*b, set a = a - 2*b, and repeat step (i). Otherwise, go to step (iii);
+// // iii) If b ≥ 2*a, set b = b - 2*a, and repeat step (i). Otherwise, return [a,b].
+// // More examples in tests cases. Good luck
+
+// Your coworker was supposed to write a simple helper function to capitalize a string (that contains a single word) before they went on vacation.
+
+// Unfortunately, they have now left and the code they gave you doesn't work. Fix the helper function they wrote so that it works as intended (i.e. make the first character in the string "word" upper case).
+
+// Don't worry about numbers, special characters, or non-string types being passed to the function. The string lengths will be from 1 character up to 10 characters, but will never be empty.
