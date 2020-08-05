6251181024154191d310629ea147d85240e0978c
hishambokhari <hish.b92@gmail.com>
2020-08-05 13:01:29 +0100

    filter

---
+++ practice_codewars/e.js
@@ -0,0 +1,9 @@
+let users = [1,2,3,4,5,6,1,1,1,4,5,6,];
+
+//filtering the users array and saving 
+//result back in users variable
+let filUsers = users.filter((a)=>{return a !== 1})
+
+
+//logging out the result in console
+console.log(filUsers);
