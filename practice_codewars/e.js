23a2cda576c95ed00c5add6db449bf2df40a6e9a
hishambokhari <hish.b92@gmail.com>
2020-08-05 13:05:34 +0100

    unique

--- practice_codewars/e.js
+++ practice_codewars/e.js
@@ -1,9 +1,10 @@
-let users = [1,2,3,4,5,6,1,1,1,4,5,6,];
+let users = [1,2,3,4,5,6,1,1,1,4,5,6,"john","jane","tom"];
 
 //filtering the users array and saving 
 //result back in users variable
-let filUsers = users.filter((a)=>{return a !== 1})
-
+ function uniq(value, index, self){
+   return self.indexOf(value) === index
+ }
+ console.log(users.filter(uniq))
 
 //logging out the result in console
-console.log(filUsers);
