16831508d9857bd827024cbd97ead0cfdbdef1be
hishambokhari <hish.b92@gmail.com>
2020-08-05 13:08:38 +0100

    practice unique

--- practice_codewars/e.js
+++ practice_codewars/e.js
@@ -1,10 +1,20 @@
-let users = [1,2,3,4,5,6,1,1,1,4,5,6,"john","jane","tom"];
+// let users = [1,2,3,4,5,6,1,1,1,4,5,6,"john","jane","tom"];
 
-//filtering the users array and saving 
-//result back in users variable
- function uniq(value, index, self){
-   return self.indexOf(value) === index
- }
- console.log(users.filter(uniq))
+// //filtering the users array and saving 
+// //result back in users variable
+//  function uniq(value, index, self){
+//    return self.indexOf(value) === index
+//  }
+//  console.log(users.filter(uniq))
 
-//logging out the result in console
+// //logging out the result in console
+
+
+
+let multiple = [1,1,1,1,1,1,2,2,2,2,3,4,5,6,7,8,9]
+
+function unique(value, index, self){
+  return self.indexOf(value) === index
+}
+
+console.log(multiple.filter(unique))
