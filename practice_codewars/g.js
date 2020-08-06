67ca19ad10218d731ac7e5d1356517412484f22e
hishambokhari <hish.b92@gmail.com>
2020-08-06 12:03:35 +0100

    recursion

--- practice_codewars/g.js
+++ practice_codewars/g.js
@@ -1,26 +1,37 @@
-// Binary Search
+// // Binary Search
 
-let array = [1,3,2,5,7,4]
-array.sort(function(a,b){return a - b})
+// let array = [1,3,2,5,7,4]
+// array.sort(function(a,b){return a - b})
 
-function BinarySearch(arr, value){
-  let high = arr.length - 1
-  let low = 0
-  let mid = 0
+// function BinarySearch(arr, value){
+//   let high = arr.length - 1
+//   let low = 0
+//   let mid = 0
 
-  while (low <= high){
-    mid = Math.floor((high+low)/2)
-    if (value == arr[mid]){
-      return arr[mid]
-    }
-    else if (value > arr[mid]){
-      low = mid + 1
-    } else {
-      high = mid - 1
-    }
+//   while (low <= high){
+//     mid = Math.floor((high+low)/2)
+//     if (value == arr[mid]){
+//       return arr[mid]
+//     }
+//     else if (value > arr[mid]){
+//       low = mid + 1
+//     } else {
+//       high = mid - 1
+//     }
+//   }
+//   return - 1
+// }
+
+// let search = BinarySearch(array, 10)
+// console.log(search)
+
+
+//Recursion
+//factorial
+function recursiveFunc(n){
+  if (n === 2){
+    return 2
   }
-  return - 1
+  return n * recursiveFunc(n-1)
 }
-
-let search = BinarySearch(array, 10)
-console.log(search)
+console.log(recursiveFunc(5))
