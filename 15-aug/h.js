4f1b372d3a458cfbd792309a72b7d567a9f70c33
hishambokhari <hish.b92@gmail.com>
2020-08-15 20:44:45 +0100

    filter function

--- 15-aug/h.js
+++ 15-aug/h.js
@@ -2,16 +2,10 @@
 
 // reject([1, 2, 3, 4, 5, 6], (n) => n % 2 === 0)  =>  [1, 3, 5]
 
-function reject(array, predicate) {
-  let newArr = []
-  for (let i =0; i <array.length;i++){
-    predicate = (array[i] % 2 !== 0 )
-   if (predicate){
-     newArr.push(array[i])
-   }
-  }
-return newArr
+function reject(array) {
+  
+  return array.filter(function(num){ return num % 2 !== 0 })
 
 }
-console.log(reject([1,2,3,4,5,6], ) )
+console.log(reject([1,2,3,4,5,6] ) )
 // Test.assertSimilar(reject([1, 2, 3, 4, 5, 6], function(num){ return num % 2 == 0; }), [1, 3, 5]);
