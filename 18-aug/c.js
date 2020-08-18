169e5942c1a545584b654a9b34f6c93a4298ce0a
hishambokhari <hish.b92@gmail.com>
2020-08-18 14:15:29 +0100

    another way

--- 18-aug/c.js
+++ 18-aug/c.js
@@ -1,11 +1,25 @@
 //remove duplicates
 
-let a = [1,2,5,2,1,8]
-let b = [];
+// let a = [1,2,5,2,1,8]
+// let b = [];
 
-for (let i = 0; i < a.length;i++){
-  if(b.indexOf(a[i]) === -1){
-      b.push(a[i])
+// for (let i = 0; i < a.length;i++){
+//   if(b.indexOf(a[i]) === -1){
+//       b.push(a[i])
+//   }
+// }
+// console.log(b)
+
+let a = [1,2,5,2,1,8]
+let b = []
+let _temp;
+
+a.sort(function(a,b){return a - b})
+
+for(let i = 0; i < a.length;i++){
+  if (a[i] !==  _temp){
+    b.push(a[i]);
+    _temp = a[i]
   }
 }
 console.log(b)
