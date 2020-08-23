807db5437d67e6e9c66e44559b6c0c20135eca27
hishambokhari <hish.b92@gmail.com>
2020-08-23 13:03:38 +0100

    class constructor

--- 22-aug/g.js
+++ 22-aug/g.js
@@ -1,15 +1,26 @@
-class Person {
-  constructor(name,age) {
-  this.name = name;
-  this.age = age;
-  this.info =  `${this.name}s age is ${this.age}`
-  }
+// class Person {
+//   constructor(name,age) {
+//   this.name = name;
+//   this.age = age;
+//   this.info =  `${this.name}s age is ${this.age}`
+//   }
   
-}
+// }
 
-let information = new Person('john', 34)
-console.log(information.info)
+// let information = new Person('john', 34)
+// console.log(information.info)
 
 // var john = new Person('john', 34)
 // Test.assertEquals(john.info, 'johns age is 34')
 
+class Cat extends Animal {
+  
+  constructor(name,speak){
+    super(name, speak)
+    this.name = name
+    this.speak = function(){
+      return `${name} meows.`
+    }
+  }
+  
+}
