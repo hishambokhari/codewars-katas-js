44c3b5f865d667710624c55cbf9cefaf4dcfa30a
hishambokhari <hish.b92@gmail.com>
2020-08-23 11:05:43 +0100

    age

--- 22-aug/f.js
+++ 22-aug/f.js
@@ -6,6 +6,14 @@ function Book(title, author, year){
   
 }
 
+//get age
+
+Book.prototype.getAge = function(){
+  const years = new Date().getFullYear() - this.year;
+  return `${this.title} is ${years} years old`
+}
+
+
 //get summary
 Book.prototype.getSummary = function(){
   return `${this.title} was written by ${this.author} in ${this.year}`;
@@ -14,4 +22,4 @@ Book.prototype.getSummary = function(){
 const book1 = new Book('Book One', 'John Doe', '2013');
 const book2 = new Book('Book two', 'tom', '2016');
 
-console.log(book2).getSummary;
+console.log(book2.getAge());
